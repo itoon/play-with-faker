9ab2cf75f096e00a3de3f1c131cd845d9f29a61f
Songklod Saengvoratip <songklod@marvelous.studio>
2025-03-01 23:38:54 +0700

    fix: add using faker for seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,59 @@
 import prisma from "~/lib/prisma";
+import { faker } from "@faker-js/faker";
 
 async function main() {
-  await prisma.user.upsert({
-    where: { email: "songklod@codeventure.app" },
-    update: {},
-    create: {
-      email: "songklod@codeventure.app",
-      name: "Songklod",
-      Post: {
-        create: {
-          title: "CodeVenture a gamify coding education for kids",
-          content: "https://codeventure.app",
-          published: true,
-        },
-      },
+  await prisma.post.deleteMany();
+  await prisma.user.deleteMany();
+
+  const NAME_DIVIDER = "|||";
+
+  function generateFullName() {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    return `${firstName}${NAME_DIVIDER}${lastName}`;
+  }
+
+  const seedUsers = faker.helpers
+    .uniqueArray(generateFullName, 10)
+    .map((name) => {
+      const [firstName, lastName] = name.split(NAME_DIVIDER);
+      return {
+        email: faker.internet.email({ firstName, lastName }),
+        name: faker.person.fullName({ firstName, lastName }),
+      };
+    });
+
+  await prisma.user.createMany({
+    data: seedUsers,
+  });
+
+  const exsitUsers = await prisma.user.findMany({
+    select: {
+      id: true,
     },
+  });
+
+  const authorIds = [];
+  for (const user of exsitUsers) {
+    const numberOfPosts = faker.number.int({ min: 1, max: 10 });
+    for (let i = 0; i < numberOfPosts; i++) {
+      authorIds.push(user.id);
+    }
+  }
+
+  const newPostData = authorIds.map((id) => {
+    return {
+      title: faker.hacker.noun(),
+      content: faker.hacker.phrase(),
+      published: faker.datatype.boolean({
+        probability: 0.8,
+      }),
+      authorId: id,
+    };
+  });
+
+  await prisma.post.createMany({
+    data: newPostData,
   });
 }
 
