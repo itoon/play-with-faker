df46b5f3c2a576953b15d6481f7963c1bb282b1c
Songklod Saengvoratip <songklod@marvelous.studio>
2025-03-01 17:07:34 +0700

    add: items table with faker

--- app.vue
+++ app.vue
@@ -33,6 +33,56 @@
         </tbody>
       </table>
     </div>
+    <div class="mt-8 overflow-x-auto rounded-lg shadow">
+      <table class="min-w-full divide-y divide-gray-200">
+        <thead class="bg-gray-50">
+          <tr>
+            <th
+              scope="col"
+              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Item Name
+            </th>
+            <th
+              scope="col"
+              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Quantity
+            </th>
+            <th
+              scope="col"
+              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Price
+            </th>
+            <th
+              scope="col"
+              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Total
+            </th>
+          </tr>
+        </thead>
+        <tbody class="bg-white divide-y divide-gray-200">
+          <tr v-for="item in items" :key="item.name">
+            <td
+              class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
+            >
+              {{ item.name }}
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+              {{ item.quantity }}
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+              ${{ Number(item.price).toFixed(2) }}
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+              ${{ (Number(item.price) * item.quantity).toFixed(2) }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -47,4 +97,17 @@ const form = ref({
   zipCode: faker.location.zipCode(),
   streetAddress: faker.location.streetAddress(),
 });
+
+const items = ref([
+  {
+    name: faker.commerce.productName(),
+    quantity: faker.number.int({ min: 1, max: 10 }),
+    price: faker.commerce.price({ min: 1, max: 100 }),
+  },
+  {
+    name: faker.commerce.productName(),
+    quantity: faker.number.int({ min: 1, max: 10 }),
+    price: faker.commerce.price({ min: 1, max: 100 }),
+  },
+]);
 </script>
