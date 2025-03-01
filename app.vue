7b6bbfbf285b69455a63e09da88ff264b094e61e
Songklod Saengvoratip <songklod@marvelous.studio>
2025-03-01 16:49:10 +0700

    add: faker with person information

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-bold">Hello Nuxt and Faker</h1>
+    {{ form }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { faker } from "@faker-js/faker";
+const form = ref({
+  name: faker.person.fullName(),
+  email: faker.internet.email(),
+  country: faker.location.country(),
+  province: faker.location.state(),
+  city: faker.location.city(),
+});
+</script>
